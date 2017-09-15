1b5eb309e3bfbd20f07ad469f7313cec595c934d
ziyi <ziyi.huang@ceasia.com.cn>
2017-09-15 14:09:26 +0800

    init project

---
+++ config/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../src/index.tsx"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../server/public")
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader"},
+
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}
