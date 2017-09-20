7bc08ead163e6051649a0b874fa782c82c9a4df7
Echohzy <1820123540@qq.com>
2017-09-20 23:04:52 +0800

    add react-router

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 
 
-export interface HelloProps { compiler: string;  framework: string; }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+
+export const Hello = () => <h1>Hello!</h1>;
 
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+
+
+
+export const Home = () => <h1>Home!</h1>;
