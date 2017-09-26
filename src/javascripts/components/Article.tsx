6c9c9ed91b00cbaa2f791915685292b8580bf68f
Echohzy <1820123540@qq.com>
2017-09-26 22:44:01 +0800

    add redux

--- src/javascripts/components/Article.tsx
+++ src/javascripts/components/Article.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
+import axios from 'axios';
 
-export default class Article extends React.Component {
-  constructor(props: any){
+interface ArticleProps {
+  match: {
+    params: {
+      id: number
+    }
+  }
+};
+export default class Article extends React.Component<ArticleProps, any> {
+  constructor(props: ArticleProps){
     super(props);
     this.state = {
       article: {}
     };
+  }
+  componentDidMount(){
+    const { match } = this.props;
+    axios.get("/api?uri=https://news-at.zhihu.com/api/4/news/"+match.params.id)
+    .then((res)=>{
+      return res;
+    }).then((res)=>{
+      this.setState({article: res});
+    }).catch(function(){
+      
+    });
   }
   render(){
     return (
--- src/javascripts/index.tsx
+++ src/javascripts/index.tsx
@@ -1,18 +1,32 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import { combineReducers, applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { routerReducer } from "react-router-redux";
 import '../stylesheets/font-awesome.min.css';
 import '../stylesheets/style.scss';
 
 import Home from "./components/Home";
+import Article from "./components/Article";
+
+let reducers = combineReducers({
+  routing: routerReducer
+});
+
+let store = createStore(reducers, applyMiddleware(thunk));
 
 const Root = ()=>(
-  <Router>
-    <div>
-      <Route exact path="/" render={()=>(<Redirect to="/home"/>)} />
-      <Route exact path="/home" component={Home} />
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router >
+      <div>
+        <Route exact path="/" render={()=>(<Redirect to="/home"/>)} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/news/:id" component={Article} />
+      </div>
+    </Router>
+  </Provider>
 )
 
 ReactDOM.render(
